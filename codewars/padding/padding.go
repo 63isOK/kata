628710fc3d69642571e4756cd250604d99d0aa19
63isOK <1876180681@qq.com>
2020-03-10 14:46:20 +0000

    feat(code) padding 绿灯
    通过性能测试看,还是标准库的写法更牛一些
    
    通过tdd的单元测试+绿灯后的性能测试,对理解标准库有很大帮助

--- codewars/padding/padding.go
+++ codewars/padding/padding.go
@@ -5,7 +5,53 @@ import (
 	"strconv"
 )
 
-func padding(i, w int) []byte {
+func padding(i, w int) string {
 	flag := fmt.Sprintf("%%0%ds", w)
-	return []byte(fmt.Sprintf(flag, strconv.Itoa(i)))
+	return fmt.Sprintf(flag, strconv.Itoa(i))
+}
+
+func paddingNoStd(i, w int) string {
+	buf := [20]byte{}
+	index := len(buf) - 1
+
+	for i > 0 || w > 0 {
+		w--
+		c := byte('0' + i%10)
+		buf[index] = c
+
+		index--
+		i /= 10
+	}
+
+	return string(buf[index+1:])
+}
+
+func paddingNoStd2(i, w int) string {
+	buf := [20]byte{}
+	index := len(buf) - 1
+
+	for i > 0 || w > 0 {
+		w--
+		buf[index] = byte('0' + i%10)
+
+		index--
+		i /= 10
+	}
+
+	return string(buf[index+1:])
+}
+
+func paddingStd(i, w int) string {
+	var b [20]byte
+	bp := len(b) - 1
+	for i >= 10 || w > 1 {
+		w--
+		q := i / 10
+		b[bp] = byte('0' + i - q*10)
+		bp--
+		i = q
+	}
+	b[bp] = byte('0' + i)
+
+	return string(b[bp:])
 }
--- codewars/padding/padding_test.go
+++ codewars/padding/padding_test.go
@@ -1,7 +1,6 @@
 package padding
 
 import (
-	"reflect"
 	"testing"
 )
 
@@ -9,18 +8,68 @@ func TestPadding(t *testing.T) {
 
 	samples := []struct {
 		i, w int
-		out  []byte
+		out  string
+		fun  func(int, int) string
 	}{
-		{2019, 3, []byte{'2', '0', '1', '9'}},
-		{2019, 5, []byte{'0', '2', '0', '1', '9'}},
-		{2019, -1, []byte{'2', '0', '1', '9'}},
+		{2018, 3, "2018", padding},
+		{2017, 5, "02017", padding},
+		{2016, -1, "2016", padding},
+		{2015, 3, "2015", paddingNoStd},
+		{2014, 5, "02014", paddingNoStd},
+		{2013, -1, "2013", paddingNoStd},
+		{2012, 3, "2012", paddingStd},
+		{2011, 5, "02011", paddingStd},
+		{2010, -1, "2010", paddingStd},
+		{2009, 3, "2009", paddingNoStd2},
+		{2008, 5, "02008", paddingNoStd2},
+		{2007, -1, "2007", paddingNoStd2},
 	}
 
 	for _, x := range samples {
-		got := padding(x.i, x.w)
+		got := x.fun(x.i, x.w)
 		want := x.out
-		if !reflect.DeepEqual(want, got) {
+		if want != got {
 			t.Fatalf("want:%v, got: %v", want, got)
+		}
+	}
+}
+
+var samples = []struct {
+	I, W int
+}{
+	{2018, 3},
+	{2017, 5},
+	{2016, -1},
+}
+
+func BenchmarkPadding(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, x := range samples {
+			padding(x.I, x.W)
+		}
+	}
+}
+
+func BenchmarkPaddingNoStd(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, x := range samples {
+			paddingNoStd(x.I, x.W)
+		}
+	}
+}
+
+func BenchmarkPaddingNoStd2(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, x := range samples {
+			paddingNoStd2(x.I, x.W)
+		}
+	}
+}
+
+func BenchmarkPaddingStd(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		for _, x := range samples {
+			paddingStd(x.I, x.W)
 		}
 	}
 }
