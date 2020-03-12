0f60b4ee4aca10e4d6edaf50009e992e303f0cb9
63isOK <1876180681@qq.com>
2020-03-12 05:59:45 +0000

    feat(code) : 括号匹配绿灯

--- codewars/parentheses/parentheses.go
+++ codewars/parentheses/parentheses.go
@@ -1,5 +1,15 @@
 package parentheses
 
+import (
+	"strings"
+)
+
 func valid(str string) bool {
-	return false
+	newStr := strings.ReplaceAll(str, "()", "")
+	for str != newStr && len(newStr) != 0 {
+		str = newStr
+		newStr = strings.ReplaceAll(str, "()", "")
+	}
+
+	return len(newStr) == 0
 }
