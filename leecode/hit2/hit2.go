dee605dcee1e9dca67600b8d2d402a322e209831
63isOK <spec-rfc@outlook.com>
2021-05-22 14:32:02 +0800

    feat(hit2): 将查找当前值和目标值之间的差,改为从hash表中读
    改变了求解问题的范围,本质上是改进了暴力求解法中,第二层遍历的复杂度,将O(n)改为了O(1)

--- leecode/hit2/hit2.go
+++ leecode/hit2/hit2.go
@@ -1,5 +1,6 @@
 package hit2
 
+// Hit2 is a brute force hit
 func Hit2(arr []int, target int) []int {
 	var i, j int
 	var one, two int
@@ -13,4 +14,19 @@ func Hit2(arr []int, target int) []int {
 
 FOUND:
 	return []int{i, j + i + 1}
+}
+
+func Hit2HashMap(arr []int, target int) []int {
+	hash := make(map[int]int, len(arr))
+	for i, v := range arr {
+		hash[v] = i
+	}
+
+	for i, v := range arr {
+		if j, ok := hash[target-v]; ok {
+			return []int{i, j}
+		}
+	}
+
+	return []int{}
 }
--- leecode/hit2/hit2_test.go
+++ leecode/hit2/hit2_test.go
@@ -7,9 +7,17 @@ import (
 )
 
 func TestHit2(t *testing.T) {
+	hit2(t, Hit2)
+}
+
+func TestHit2Hash(t *testing.T) {
+	hit2(t, Hit2HashMap)
+}
+
+func hit2(t *testing.T, impl func([]int, int) []int) {
 	input_arr := []int{3, 4, 2}
 	input_target := 5
 	want := []int{0, 2}
-	got := Hit2(input_arr, input_target)
+	got := impl(input_arr, input_target)
 	assert.Equal(t, got, want)
 }
