6d926011a95025b8282e243d0909561f7f05d10c
63isNotOK <45553405+63isOK@users.noreply.github.com>
2021-05-25 11:55:18 +0800

    Hit3 双游标解法 (#11)
    * feat(hit3): 分析
    
    * feat(hit3): O(n*n)的实现
    
    * feat(hit3): 第三种解法
    
    * feat(hit3): 去重,未完成
    
    * feat(hit3): 排序之后,双游标解法,效果很理想
    
    后续还可以优化
    
    * fix(md): 修复md lint

---
+++ leecode/hit3/hit3_test.go
@@ -0,0 +1,50 @@
+package hit3
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHit3(t *testing.T) {
+	input_arr := []int{3, 4, 2, -1, -1, 0, -2, 1, 0, 0, 0}
+	want := [][]int{
+		[]int{-2, -1, 3},
+		[]int{-2, 0, 2},
+		[]int{-1, -1, 2},
+		[]int{-1, 0, 1},
+		[]int{0, 0, 0}}
+	got := hit3(input_arr)
+	assert.ElementsMatch(t, got, want)
+}
+
+func TestHit32(t *testing.T) {
+	input_arr := []int{-1, 0, 1, 2, -1, -4}
+	want := [][]int{
+		[]int{-1, -1, 2},
+		[]int{-1, 0, 1}}
+	got := hit32(input_arr)
+	assert.ElementsMatch(t, got, want)
+}
+
+func TestHit3Web(t *testing.T) {
+	input_arr := []int{-1, 0, 1, 0}
+	want := [][]int{[]int{-1, 0, 1}}
+	got := hit3(input_arr)
+	assert.ElementsMatch(t, got, want)
+}
+
+func BenchmarkHit3(b *testing.B) {
+	input_arr := []int{3, 4, 2, -1, -1, 0, -2, 1, 0, 0, 0}
+	for i := 0; i < b.N; i++ {
+		hit3(input_arr)
+	}
+}
+
+func TestHit3Point(t *testing.T) {
+	input_arr := []int{0, 0, 0}
+	want := [][]int{
+		[]int{0, 0, 0}}
+	got := hit3Point(input_arr)
+	assert.ElementsMatch(t, got, want)
+}
